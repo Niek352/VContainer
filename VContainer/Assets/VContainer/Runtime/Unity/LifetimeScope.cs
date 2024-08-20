11945f6822234c96a4ca1238b4879dd04cbee9ec
Niek352 <oavelasdqwe8@gmail.com>
2024-08-20 10:15:26 +0300

    update scope

--- VContainer/Assets/VContainer/Runtime/Unity/LifetimeScope.cs
+++ VContainer/Assets/VContainer/Runtime/Unity/LifetimeScope.cs
@@ -168,7 +168,26 @@ namespace VContainer.Unity
             DisposeCore();
         }
 
-        protected virtual void Configure(IContainerBuilder builder) { }
+        protected virtual void Configure(IContainerBuilder builder)
+        {
+            foreach (var installer in _scriptableObjectInstallers)
+            {
+                if (Parent != null)
+                {
+                    Parent.Container.Inject(installer);   
+                }
+                installer.Install(builder);
+            }
+            
+            foreach (var installer in _monoInstallers)
+            {
+                if (Parent != null)
+                {
+                    Parent.Container.Inject(installer);   
+                }
+                installer.Install(builder);
+            }
+        }
 
         public void Dispose()
         {
